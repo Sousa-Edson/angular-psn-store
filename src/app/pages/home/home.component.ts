aaeabcaf8a9bb2f53cb57bb55e61d261201f013f
Edson Sousa <Sousa-Edson>
2024-12-10 23:40:57 -0300

    adiciona mais games

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,9 +10,11 @@ import { CardComponent } from "../../components/card/card.component";
 })
 export class HomeComponent {
   games = [
+    { gameCover: 'assets/ac-cover.jpg', gameLabel: 'BEST OF YEAR', gameType: 'DIGITAL | PS5', gamePrice: 'R$ 369,99' },
     { gameCover: 'assets/bt-1.jpg', gameLabel: '', gameType: 'DIGITAL | PS4', gamePrice: 'R$ 129,99' },
     { gameCover: 'assets/bt-4.jpg', gameLabel: 'EXCLUSIVE', gameType: 'DISC | PS5', gamePrice: 'R$ 269,99' },
-    { gameCover: 'assets/ac-cover.jpg', gameLabel: 'BEST OF YEAR', gameType: 'DIGITAL | PS5', gamePrice: 'R$ 369,99' },
+    { gameCover: 'assets/bt-5.jpg', gameLabel: '', gameType: 'DISC | PS5', gamePrice: 'R$ 289,99' },
+    { gameCover: 'assets/bt-bad-company-2.jpg', gameLabel: 'BEST OF YEAR', gameType: 'DIGITAL | PS5', gamePrice: 'R$ 89,99' },
     { gameCover: 'assets/bt-hardline.jpg', gameLabel: 'NEW', gameType: 'DIGITAL | PS3 PS4 PS5', gamePrice: 'R$ 369,99' },
   ];
 }

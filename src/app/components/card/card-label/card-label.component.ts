3b410b27f353ce6593d0055e9679a119438875dc
Edson Sousa <Sousa-Edson>
2024-12-10 22:03:40 -0300

    cria projeto limpo e adiciona o tailwind

---
+++ src/app/components/card/card-label/card-label.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-label',
+  standalone: true,
+  imports: [],
+  templateUrl: './card-label.component.html',
+  styleUrl: './card-label.component.scss'
+})
+export class CardLabelComponent {
+
+}
